d94baefbc0a70053c095e7332b0c09bce030b918
Jonatha Azevedo <johnmasterchip@gmail.com>
2018-09-21 17:20:53 -0300

    Function that will return a random UA

---
+++ codes/get_random_ua.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def get_random_ua():
+    random_ua = ''
+    ua_file = 'ua_file.txt'
+    try:
+        with open(ua_file) as f:
+            lines = f.readlines()
+        if len(lines) > 0:
+            prng = np.random.RandomState()
+            index = prng.permutation(len(lines) - 1)
+            idx = np.asarray(index, dtype=np.integer)[0]
+            random_proxy = lines[int(idx)]
+    except Exception as ex:
+        print('Exception in random_ua')
+        print(str(ex))
+    finally:
+        return random_ua
