767b383274e6928e6bfe35fdad04805a2c5496a5
Akash Das <mfs.akash@gmail.com>
2020-03-05 19:31:12 +0530

    Fixed a mismatching variable name
    Changed `random_proxy` in the try block to `random_ua` as originally intended

--- codes/get_random_ua.py
+++ codes/get_random_ua.py
@@ -11,7 +11,7 @@ def get_random_ua():
             prng = np.random.RandomState()
             index = prng.permutation(len(lines) - 1)
             idx = np.asarray(index, dtype=np.integer)[0]
-            random_proxy = lines[int(idx)]
+            random_ua = lines[int(idx)]
     except Exception as ex:
         print('Exception in random_ua')
         print(str(ex))
